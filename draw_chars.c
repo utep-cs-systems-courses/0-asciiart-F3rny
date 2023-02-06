4884383044d4c10826d62b762dc0b2d7398753e6
student <fhremes@miners.utep.edu>
2023-02-06 12:52:43 -0700

    Updated font to 5 x 7

--- draw_chars.c
+++ draw_chars.c
@@ -4,10 +4,10 @@
 void print_char_11x16(char c)
 {
   c -= 0x20;
-  for (char row = 0; row < 11; row++) {
-    unsigned short rowBits = font_11x16[c][row];
-    for (char col = 0; col < 16; col++) {
-      unsigned short colMask = 1 << (15-col); /* mask to select bit associated with bit */
+  for (char row = 0; row < 5 ; row++) {
+    unsigned char rowBits = font_5x7 [c][row];
+    for (char col = 0; col < 7; col++) {
+      unsigned char colMask = 1 << (6 -col); /* mask to select bit associated with bit */
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
     putchar('\n');
